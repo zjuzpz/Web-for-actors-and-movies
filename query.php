3cfa07251469c7c38d4255294fe8863c4c419a81
root <Peizhe>
2015-10-25 06:33:26 -0700

    query

--- query.php
+++ query.php
@@ -6,11 +6,11 @@
 	</title>
     </head>
     <body>
-	<h5>"Input your query into the following box"</h5>
-	<h5>"Example: select * from actor where id = 10"<h5/>
+	<h3>"Input your query into the following box"</h3>
+	<h3>"Example: select * from actor where id = 10"<h3/>
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
  	   Query:<br> 
-	   <textarea name="query" rows=5 cols=30></textarea>
+	   <textarea style="font-size:18px" name="query" rows=5 cols=50></textarea>
   	   <input type="submit">
 	</form>
 
@@ -30,13 +30,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   mysql_select_db("movie", $db_connection);
 //  $query = "select count(*) from actor";
   $rs = mysql_query($query, $db_connection);
-  print "Answer: ";
-  while($row = mysql_fetch_row($rs)){
-    for($i=0;$i<count($row);$i++){
-      $num = $row[$i];
-      print "$num ";
+  print "Answer:<br/> ";
+  print "<table border=3> ";
+    $len = mysql_num_fields($rs);
+    print "<tr>";
+    for($i=0;$i<$len;$i++){
+      print "<td style='text-align:center'>".mysql_field_name($rs,$i)."</td>";
     }
-  }
+    print "</tr>";
+    while($row = mysql_fetch_row($rs)){
+    print "<tr>";
+      for($i=0;$i<count($row);$i++){
+        $val = $row[$i];
+        if($val){
+          print "<td>"."$val "."</td>";
+        } else {print "<td>N/A</td>";}  
+      } 
+    print "</tr>";
+    }
+  print "</table>";
+
   mysql_close($db_connection);
 ?>
     </body>
