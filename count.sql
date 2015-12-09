a300bedbb49b05ad70636c4fc15a37f600d7238c
zjuzpz <zjuzpz@gmail.com>
2015-12-08 18:21:18 -0800

    change table name

--- count.sql
+++ count.sql
@@ -1,11 +1,11 @@
-select count(*) from actor;
-select count(*) from movie;
-select count(*) from director;
-select count(*) from movieGenre;
-select count(*) from movieDirector;
-select count(*) from movieActor;
-select count(*) from review;
-select * from maxPersonID;
-select * from maxMovieID;
+select count(*) from Actor;
+select count(*) from Movie;
+select count(*) from Director;
+select count(*) from MovieGenre;
+select count(*) from MovieDirector;
+select count(*) from MovieActor;
+select count(*) from Review;
+select * from MaxPersonID;
+select * from MaxMovieID;
 
 
